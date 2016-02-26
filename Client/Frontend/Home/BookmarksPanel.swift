abb47d85860f45182cca1fff46bc7e9be74883a7
Richard Newman <rnewman@twinql.com>
2016-02-26 11:20:24 -0800

    Bug 1251306 - Part 4: switch between synced and unsynced bookmark views.
    We expose a model factory that forks into either (local + mirror) or
    (buffer + mirror) for Mobile Bookmarks and Desktop Bookmarks
    respectively.
    
    This allows us to show and edit local bookmarks while offering a
    read-only view of the current incomplete server data.

--- Client/Frontend/Home/BookmarksPanel.swift
+++ Client/Frontend/Home/BookmarksPanel.swift
@@ -238,7 +238,7 @@ class BookmarksPanel: SiteTableViewController, HomePanel {
             let nextController = BookmarksPanel()
             nextController.parentFolders = parentFolders + [source.current]
             nextController.bookmarkFolder = folder
-            nextController.source = source
+            nextController.source = BookmarksModel(modelFactory: source.modelFactory, root: folder)
             nextController.homePanelDelegate = self.homePanelDelegate
             nextController.profile = self.profile
             self.navigationController?.pushViewController(nextController, animated: true)
